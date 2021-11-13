92cac6724c4d8aab0d8c36eb7ae47771a4f91613
AlkarimJ1997 <ajiwa97@gmail.com>
2021-11-13 01:19:26 -0800

    Populated DB, finished GET ALL endpoint

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const url = require('url');
 const mysql = require('mysql');
+const fs = require('fs');
 
 const app = express();
+const port = 3000;
 
 // Endpoints
 const getEndPoint = '/API/v1/questions';
 
-const port = process.env.PORT || 8888;
+// const port = process.env.PORT || 8888;
 const db = mysql.createConnection({
     host: 'comp4537s2.mysql.database.azure.com',
     user: 's2',
@@ -32,8 +34,41 @@ app.get(getEndPoint, (req, res) => {
                 throw err;
             }
             res.statusCode = 200;
-            res.end(`{"questions":${JSON.stringify(result)}}`);
+            res.header('Content-Type', 'application/json')
+            res.end(JSON.stringify(result));
         })
     })
+})
+
+// DO NOT UNCOMMMENT, ALREADY POPULATED DB
+
+// app.get("/", (req, res) => {
+//     fs.readFile('./assets/questions.json', (err, data) => {
+//         if (err) throw err;
+//         const questionArr = JSON.parse(data);
+
+//         let sql = "INSERT INTO question(question, choice1, choice2, choice3, choice4, answer, category) VALUES ";
+//         for (let i = 0; i < questionArr.length - 1; i++) {
+//             let q = questionArr[i];
+//             sql += `("${q.question}","${q.choice1}","${q.choice2}","${q.choice3}","${q.choice4}","${q.answer}","${q.category}"),`
+//         }
+//         let final = questionArr[questionArr.length - 1];
+//         sql += `("${final.question}","${final.choice1}","${final.choice2}","${final.choice3}","${final.choice4}","${final.answer}","${final.category}");`
+
+//         db.connect(() => {
+//             db.query(sql, (err, result) => {
+//                 if (err) {
+//                     console.error(err);
+//                     throw err;
+//                 }
+//                 res.statusCode = 200;
+//                 res.end(`Successfully inserted data: ${result}`);
+//             })
+//         })
+//     })
+// })
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 })
 
